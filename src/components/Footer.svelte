5349faff6ec0423252b07dacd00065af2257b7e6
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2021-11-01 10:08:51 +0900

    pnpm, vite, svelte, ts, all set up

---
+++ src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<script>
+
+</script>
+
+<footer class="footer">
+    <span>This is footer</span>
+</footer>
+
+<style>
+
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<div>
+    <span>This is header</span>
+</div>
+
+
+
+<style>
+
+</style>
